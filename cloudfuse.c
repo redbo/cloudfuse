57a4969ef39427e23232c8ecbcbf90b6f57343d7
Mike Barton <mike@weirdlooking.com>
2011-08-12 10:55:01 +0000

    arrange code back to my preference ;)

--- cloudfuse.c
+++ cloudfuse.c
@@ -16,7 +16,27 @@
 #include <stddef.h>
 #include "cloudfsapi.h"
 #include "config.h"
-#include "cloudfuse.h"
+
+
+#define OPTION_SIZE 1024
+
+static int cache_timeout;
+
+typedef struct dir_cache
+{
+  char *path;
+  dir_entry *entries;
+  time_t cached;
+  struct dir_cache *next, *prev;
+} dir_cache;
+static dir_cache *dcache;
+static pthread_mutex_t dmut;
+
+typedef struct
+{
+  int fd;
+  int flags;
+} openfile;
 
 
 static void dir_for(const char *path, char *dir)
@@ -396,6 +416,20 @@ char *get_home_dir()
     return home;
   return "~";
 }
+
+static struct options {
+    char username[OPTION_SIZE];
+    char api_key[OPTION_SIZE];
+    char cache_timeout[OPTION_SIZE];
+    char authurl[OPTION_SIZE];
+    char use_snet[OPTION_SIZE];
+} options = {
+    .username = "",
+    .api_key = "",
+    .cache_timeout = "600",
+    .authurl = "https://auth.api.rackspacecloud.com/v1.0",
+    .use_snet = "false",
+};
 
 int parse_option(void *data, const char *arg, int key, struct fuse_args *outargs)
 {
