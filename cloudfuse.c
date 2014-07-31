redbo/cloudfuse

commit de800c535321aef7b5c06bb11ceedfda9daf4c4b

Fixed truncate on open (O_WRONLY) and file append bugs: https://github.com/redbo/cloudfuse/issues/62 https://github.com/redbo/cloudfuse/issues/57

cloudfuse.c
@@ -270,9 +270,9 @@ static int cfs_open(const char *path, struct fuse_file_info *info)
 {
   FILE *temp_file = tmpfile();
   dir_entry *de = path_info(path);
-  if (!(info->flags & O_WRONLY))
+  if (!(info->flags & O_TRUNC))
   {
-    if (!cloudfs_object_write_fp(path, temp_file))
+    if (!cloudfs_object_write_fp(path, temp_file) && !(info->flags & O_CREAT))
     {
       fclose(temp_file);
       return -ENOENT;
@@ -285,6 +285,7 @@ static int cfs_open(const char *path, struct fuse_file_info *info)
   of->flags = info->flags;
   info->fh = (uintptr_t)of;
   info->direct_io = 1;
+  info->nonseekable = 1; // requires fuse>=2.8
   return 0;
 }
 

commit 1a595c1408df274fc12f9d99851ef7c7decaaed2

Fixed bug with seeking (nonseekable fuse option) Bug was introduced in de800c535321aef7b5c06bb11ceedfda9daf4c4b

cloudfuse.c
@@ -285,7 +285,6 @@ static int cfs_open(const char *path, struct fuse_file_info *info)
   of->flags = info->flags;
   info->fh = (uintptr_t)of;
   info->direct_io = 1;
-  info->nonseekable = 1; // requires fuse>=2.8
   return 0;
 }
 