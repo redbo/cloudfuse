6ac76fdd889cfaa52c1aade69e85af9da5db73fd
Am1GO <amigo.elite@gmail.com>
2015-08-24 12:51:12 +0300

    A little bit more confident inodes indication

--- cloudfsapi.c
+++ cloudfsapi.c
@@ -42,8 +42,8 @@ static struct statvfs statcache = {
   .f_bfree = INT_MAX,
   .f_bavail = INT_MAX,
   .f_files = INT_MAX,
-  .f_ffree = 0,
-  .f_favail = 0,
+  .f_ffree = INT_MAX,
+  .f_favail = INT_MAX,
   .f_namemax = INT_MAX
 };
 
@@ -126,7 +126,7 @@ static size_t header_dispatch(void *ptr, size_t size, size_t nmemb, void *stream
     if (!strncasecmp(head, "x-account-bytes-used", size * nmemb))
       statcache.f_bfree = statcache.f_bavail = statcache.f_blocks - strtoul(value, NULL, 10);
     if (!strncasecmp(head, "x-account-object-count", size * nmemb))
-      statcache.f_files = strtoul(value, NULL, 10);
+      statcache.f_ffree = statcache.f_bavail = statcache.f_files - strtoul(value, NULL, 10);
   }
   return size * nmemb;
 }
